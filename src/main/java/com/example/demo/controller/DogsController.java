f6dbefbffe889ec46ce36fc524b06b66af68d5c3
Abhishek Prasad <abhishek.prasad871@gmail.com>
2021-11-18 20:44:49 +0530

    2

--- src/main/java/com/example/demo/controller/DogsController.java
+++ src/main/java/com/example/demo/controller/DogsController.java
@@ -16,7 +16,7 @@ public class DogsController {
 
 	public DogsController() {
 		System.out.println("DogsController called");
-		System.out.println("some changess");
+		System.out.println("some changes");
 	}
 
 	private DogsService service;
